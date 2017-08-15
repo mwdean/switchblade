53882c16a656c3bbe529caef1a14fab0cd6f0312
Michael Dean <mdean@api-digital.com>
2017-08-15 16:06:39 -0500

    initial

---
+++ Switchblade/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Switchblade
+//
+//  Created by Michael Dean on 8/15/17.
+//  Copyright © 2017 Michael Dean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Switchblade/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Switchblade
+//
+//  Created by Michael Dean on 8/15/17.
+//  Copyright © 2017 Michael Dean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
